af95a9e437efca2b4a911546dde5f77f25e6b076
RAfaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-19 23:14:54 +0000

    Adding variables with their respective formats

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,6 +6,14 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            double xx= 1.23456;
+            int ii= 19;
+            Console.WriteLine($"Número real com duas casas decimais: {xx:F2}");
+            Console.WriteLine($"Percentagem com uma casa decimal: {xx:P1}");
+
+            Console.WriteLine($"Hexadecimal: {ii:X}");
+            Console.WriteLine($"Moeda: {ii:c}");
+
             string lyric = "There's a starman waiting in the sky";
             string space= @"And Im floating in a most peculiar way
             And the stars look very different today";
