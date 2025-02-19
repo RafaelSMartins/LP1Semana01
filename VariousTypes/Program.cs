5346c73ec1d7df3d1c0b572beafb0212aec47eda
RAfaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-19 22:13:34 +0000

    adding real varialbles

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -15,6 +15,10 @@ namespace VariousTypes
             char skull='\u2620';
             char musicFlatSign='\u266D';
             Console.WriteLine($"{communistSymbol}, {skull}, {musicFlatSign}");
+
+            float xp=0.90f;
+            double patience=19.8d;
+            Console.WriteLine(${xp}, {patience});
         }
     }
 }
