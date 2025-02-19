121a44e67a5eda3f6898c4ba423e2033ffb0eec5
RAfaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-19 21:52:32 +0000

    Adding int variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            long scooby=900000000000000000000000000000000000000000000000000000;
+            int a=2000000;
+            short  ornintorrinco=90;
+            Console.WriteLine($"{ornintorrinco}, {a}, {scooby}")
+        }
+    }
+}
