3f602099974568f4dba3b62ad6fc2e33940cb5b8
RAfaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-19 22:03:54 +0000

    Adding char int varibles

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,10 +6,15 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
-            long scooby=900000000000000000000000000000000000000000000000000000;
-            int a=2000000;
+            long scooby=9000000;
+            int a=200;
             short  ornintorrinco=90;
-            Console.WriteLine($"{ornintorrinco}, {a}, {scooby}")
+            Console.WriteLine($"{ornintorrinco}, {a}, {scooby}");
+
+            char communistSymbol='\u262D';
+            char skull='\u2620';
+            char musicFlatSign='\u266D';
+            Console.WriteLine($"{communistSymbol}, {skull}, {musicFlatSign}");
         }
     }
 }
