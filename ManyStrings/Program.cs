54fec376f80f32f1705f95d98851969001bb6eed
RAfaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-19 22:45:17 +0000

    adding string to ManyStrings project

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string lyric = "There's a starman waiting in the sky";
+            string space= @"And I’m floating in a most peculiar way
+            And the stars look very different today";
+            string something = "MR Penguini\t Channel\U001F427 ";
+            Console.WriteLine($"{lyric}, {space}, {something}");
+        }
+    }
+}
