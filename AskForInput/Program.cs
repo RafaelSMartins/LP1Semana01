856376f8ff197c07b9aa5f488941e8fc48f4ed85
RafaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-21 03:12:51 +0100

    Asking for number Input

---
+++ AskForInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Inserir um número inteiro: ");
+            string str = Console.ReadLine();
+            
+        }
+    }
+}
