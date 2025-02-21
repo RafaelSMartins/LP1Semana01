152fb5e7b11fa17e84c11d671ffd8070bdb3d359
RafaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-21 14:35:09 +0000

    Tentativa de fazer o codigo

---
+++ Percentagem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Insere 4 números reais entre 0 e 1:");
+
+            string str = Console.ReadLine();
+
+            float f = 0f;
+            f = float.Parse(str);
+
+            string f = $"’{0.2: p1}’"
+
+            Console.WriteLine($"{f}");
+
+
+        
+        }
+    }
+}
