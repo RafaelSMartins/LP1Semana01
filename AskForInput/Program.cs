96d2ddb37acf67d48aafaeeb14dc004dfeb80b11
RafaelSMartins <rafaelsantosmartins.dev@gmail.com>
2025-02-21 13:18:02 +0000

    Adding Using Sste Globalization

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace AskForInput
 {
@@ -6,11 +7,27 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
             Console.WriteLine("Inserir um número inteiro: ");
             string str = Console.ReadLine();
 
             int i = int.Parse(str);
-            
+
+            Console.WriteLine("Inserir um número real: ");
+            string perry = Console.ReadLine();
+
+            float f = 0f;
+            f = float.Parse(perry);
+
+            float soma = i + f;
+
+            Console.WriteLine($"soma de {i} mais {f} igual a {soma}");
+
+
+
+
+
         }
     }
 }
